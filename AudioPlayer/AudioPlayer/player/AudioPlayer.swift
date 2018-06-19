6ca0b7900ac4a6c98559781615713b483d2353a6
Jay <21jaypatel@gmail.com>
2018-06-19 13:54:15 -0400

    Make player available outside the framework

--- AudioPlayer/AudioPlayer/player/AudioPlayer.swift
+++ AudioPlayer/AudioPlayer/player/AudioPlayer.swift
@@ -52,7 +52,7 @@ public class AudioPlayer: NSObject {
     var queue: AudioItemQueue?
 
     /// The audio player.
-    var player: AVPlayer? {
+    public var player: AVPlayer? {
         didSet {
             if #available(OSX 10.11, *) {
                 player?.allowsExternalPlayback = false
@@ -75,6 +75,9 @@ public class AudioPlayer: NSObject {
                 networkEventProducer.stopProducingEvents()
                 audioItemEventProducer.stopProducingEvents()
                 qualityAdjustmentEventProducer.stopProducingEvents()
+            }
+            if #available(iOS 10.0, *) {
+                player?.automaticallyWaitsToMinimizeStalling = false
             }
         }
     }
